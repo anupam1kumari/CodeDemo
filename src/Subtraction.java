6efdd46b1def402e95b5a7a2b0b2fd27f53d05eb
anupam1kumari <kumari.anupam846@gmail.com>
2019-05-19 00:18:26 +0530

    again

---
+++ src/Subtraction.java
@@ -0,0 +1,15 @@
+
+public class Subtraction {
+int sub;
+	public int sub(int a,int b) {
+		sub=a-b;
+		return sub;
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Subtraction sub=new Subtraction();
+System.out.println("Subtraction = "+sub);
+	}
+
+}
