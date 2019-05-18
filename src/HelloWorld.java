8da3eec8b8aa10ecd2f872a9d7b03b8984911aec
anupam1kumari <kumari.anupam846@gmail.com>
2019-05-18 23:47:50 +0530

    inserted add method

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,16 @@
 
 public class HelloWorld {
-
+//sum
+	int sum=0;
+public int add(int a,int b) {
+int sum=a+b;
+return sum;
+}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+HelloWorld h=new HelloWorld();
+System.out.print("sum= "+h.add(2,3));
 	}
 
 }
